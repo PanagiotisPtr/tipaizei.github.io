cea3683fb4aef1a457459c9e5b7fb8993410c8ef
PanagiotisPtr <lPanagiotisPetridisl@gmail.com>
2016-11-18 17:22:17 +0200

    Quick Fix

---
+++ autoUpdate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash  
+echo "Parsing Data from the Internet"
+python3 parse.py
+echo "Updating index.html"
+g++ update.cpp
+./a.out
+echo "Pushing update to GitHub"
+git add .
+git commit -m "Daily Update"
+git push -f
+echo "Pushed Changes on GitHub"
