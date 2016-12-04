7b471fa2c11b9611ae0e78484f44dfa464c5e6dd
PanagiotisPtr <lPanagiotisPetridisl@gmail.com>
2016-12-04 14:12:32 +0200

    Updated autoUpdate.sh to accept user input as a commit message.

--- autoUpdate.sh
+++ autoUpdate.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 echo "Enter commit message:"
 read message
-#echo "Parsing Data from the Internet"
-#python3 parse_updated.py
-#echo "Updating index.html"
-#g++ update.cpp -std=c++11
-#./a.out
+echo "Parsing Data from the Internet"
+python3 parse_updated.py
+echo "Updating index.html"
+g++ update.cpp -std=c++11
+./a.out
 echo "Pushing update to GitHub"
 git add .
-git commit -m "Daily Update $message"
+git commit -m "	$message"
 git push -f
 echo "Pushed Changes on GitHub"
 echo "Enjoy"
+sensible-browser https://github.com/PanagiotisPtr/tipaizei.github.io
 sensible-browser http://www.tipaizei.tk
